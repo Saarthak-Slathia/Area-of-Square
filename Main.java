accaa7e39d10620b0d7ce8bbd600e2e13b9e4f8a
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2021-06-16 11:37:09 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.learnjava;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int area;
+	    int side;
+
+	    side = 5;
+	    area = side * side;
+        System.out.println("The area of a square with side of length " + side + "cm is " + area + ".");
+    }
+}
